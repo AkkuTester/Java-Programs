01cd52a618f05beb4e8fe9fa8e55f2ae1da3b6b3
ThakareSupriya <mudesupriya53@gmail.com>
2023-04-10 11:13:40 +0530

    forLoopDemo committed

---
+++ Java_InterviewPreparation/src/forLoop/ForLoopDemo.java
@@ -0,0 +1,13 @@
+package forLoop;
+
+public class ForLoopDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=0; i<=5;) {
+			System.out.println("hello");
+		}
+	}
+
+}
