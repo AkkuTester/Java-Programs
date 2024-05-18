6dc122d83105f29d4d473425d94e23d362f956d0
Akash Thakare <122626835+AkkuTester@users.noreply.github.com>
2024-05-18 19:06:09 +0530

    Update and rename Removing_all_Matching_Characters.java to Removing_Duplicates.java
    Removing_all_Matching_Characters update

---
+++ Java_InterviewPreparation/src/javaPrograms/Removing_Duplicates.java
@@ -0,0 +1,44 @@
+package javaPrograms;
+
+public class Removing_Duplicates {
+
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+		RemoveDuplicates.usingStringBuilder();
+		RemoveDuplicates.usingNormalString();
+	}
+
+	public static void usingNormalString() {
+	// Remove Duplicates using Normal String
+		
+		String s = "selenium"; 	 // selnium
+		
+		String withoutDuplicate = "";
+		HashSet<Character> set = new HashSet<>();
+		
+		for(char c : s.toCharArray()) {
+			if(set.add(c)) {
+			  withoutDuplicate = withoutDuplicate + c;
+			}
+		}
+		System.out.println(withoutDuplicate);
+	}
+	
+	
+	public static void usingStringBuilder() {
+	//Remove Duplicates using String Builder
+
+		String s = "automation Testing" ;  // automin Tesg 
+		
+		HashSet<Character> set = new HashSet<>();
+		StringBuilder sb = new StringBuilder();
+		
+		for(char c : s.toCharArray()) {	
+	    	   if(set.add(c)) {
+	      	     sb.append(c);
+	    	   }	
+		}
+		System.out.println(sb);	
+	}
+
+}
