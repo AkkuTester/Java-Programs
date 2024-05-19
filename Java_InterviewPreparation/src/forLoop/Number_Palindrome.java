f239a447bd207b676c9b88f3b8c56ccee3583e67
Akash Thakare <122626835+AkkuTester@users.noreply.github.com>
2024-05-19 16:30:14 +0530

    Update and rename ForLoopDemo.java to Number_Palindrome.java

---
+++ Java_InterviewPreparation/src/forLoop/Number_Palindrome.java
@@ -0,0 +1,22 @@
+package forLoop;
+
+public class ForLoopDemo {
+
+	public static void main(String[] args) {
+		// Reverse a number & check if it is palindrome
+
+		int n = 474 ; int rev = 0; int nCopy = n;
+		
+		while(n != 0) {
+			int rem = n%10;
+			rev = rev *10 + rem ;
+			n = n/10;
+		}
+		System.out.println(rev);
+		
+		if(rev == nCopy) {
+			System.out.println("Given no is palindrome");
+		}
+	}
+
+}
