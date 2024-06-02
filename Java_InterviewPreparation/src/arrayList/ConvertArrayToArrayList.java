1588dcf3f9c33f67c68058b475b873ed71245a63
Akash Thakare <122626835+AkkuTester@users.noreply.github.com>
2024-06-02 19:44:55 +0530

    Update ConvertArrayToArrayList.java
    added to convert set to Arraylist tor retrieve desired string

--- Java_InterviewPreparation/src/arrayList/ConvertArrayToArrayList.java
+++ Java_InterviewPreparation/src/arrayList/ConvertArrayToArrayList.java
@@ -16,4 +16,20 @@ public class ConvertArrayToArrayList {
 		System.out.println(al);
 	}
 
+	public void convert_SetToArrayList(){
+		// infogain question:  add 3 string in set and 1st string from that set
+		
+		Set<String> journeys = new HashSet<>();
+		journeys.add("login");                  
+		journeys.add("ForgotPassword"); 
+		journeys.add("ResetPassword");  
+		journeys.add("Create Folders");
+	
+		System.out.println(journeys);
+		ArrayList<String> list = new ArrayList<>(journeys);
+		
+		System.out.println("ArrayList:  " + list);
+		System.out.println(list.get(1));          //  retrieve desired string
+	}
+	
 }
